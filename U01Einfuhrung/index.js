278b8e5c92a99b66ab50dd210aff615d30f3bc29
Benji <50681275+Benji377@users.noreply.github.com>
2023-03-23 14:22:25 +0100

    New hw

---
+++ U01Einfuhrung/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+const movieRouter = require('./movie/movie.router.js');
+
+const authController = require('./auth/auth.controller');
+authController(app);
+
+app.use('/movie', movieRouter);
+app.get('/', (request, response) => response.redirect('/movie'));
+app.use(express.static(__dirname));
+app.listen(8080, () => console.log('Server listen on port 8080'));
